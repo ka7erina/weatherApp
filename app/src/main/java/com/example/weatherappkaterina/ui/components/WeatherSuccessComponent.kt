08cacbfb801594af2f7ea815396fa1d64d6fab50
Katerina Nikolovska <kostadinovska1katerina@gmail.com>
2024-04-01 18:22:52 +0200

    remove unused imports

--- app/src/main/java/com/example/weatherappkaterina/ui/components/WeatherSuccessComponent.kt
+++ app/src/main/java/com/example/weatherappkaterina/ui/components/WeatherSuccessComponent.kt
@@ -11,12 +11,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.weatherappkaterina.R
-import com.example.weatherappkaterina.domain.model.Main
-import com.example.weatherappkaterina.domain.model.Weather
 import com.example.weatherappkaterina.domain.model.WeatherData
 import com.example.weatherappkaterina.ui.theme.Blue
 
